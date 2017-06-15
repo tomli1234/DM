9cb26122071c5ae80cce31f10d7996d1244c93c2
jcqn <chao.quan@hku.hk>
2017-06-15 19:33:15 +0800

    External validation (R script)

--- External validation.r
+++ External validation.r
@@ -1,3 +1,5 @@
+rm(list=ls())
+
 # External validation of CU and UKPDS mortality model for diabetic patients
 # Using HKU data
 # Tom Li, tomli123@hku.hk, 21 Mar 2017
@@ -107,7 +109,7 @@ FUN.deciles_mean_pred <- function(data) {
       data.frame(observed, pred = mean_pred, Deciles=1:10)
 }
 
-plot_cal_bar <- function(dat.melt, colours = c('skyblue1','darkblue')){
+plot_bar <- function(dat.melt, colours = c('skyblue1','darkblue')){
       ggplot(dat.melt, aes(x = factor(Deciles), y = Risk*100, fill = Group)) + 
             geom_bar(stat = 'identity', position = 'dodge', width = 0.7) +  
             xlab("Tenth of predicted risk") + 
@@ -127,10 +129,29 @@ plot_cal_bar <- function(dat.melt, colours = c('skyblue1','darkblue')){
       
 }
 
+plot_cal_bar <- function(dat.melt, kolor = colours){
+  ggplot(dat.melt, aes(x = Deciles, y = Risk*100, fill = factor(Group, levels = c("pred", "observed")))) + 
+    geom_bar(stat = 'identity', position = 'dodge', width = 0.7, color = "black") +  
+    xlab("Tenth of predicted risk") + 
+    ylab("5 year risk (%)") + 
+    theme(axis.title.x = element_text(size=14), 
+          axis.title.y = element_text(size=14), 
+          axis.text.y = element_text(size=12)) + 
+    scale_x_discrete() + 
+    scale_y_continuous(breaks=seq(0, 100, 10)) + 
+    scale_fill_manual(values = c('pred' = kolor[1], "observed" = kolor[2]),
+                      labels=c("observed" = 'Observed','pred' = 'Predicted')) +
+    theme_minimal() + 
+    theme(legend.position="bottom", 
+          legend.title=element_blank(), 
+          legend.text = element_text(size = 12, face = "bold"),
+          panel.grid.major.y = element_line( size=.1, color="grey"))
+}
+
 external_validation <- function(outcome, CU_UKPDS, sex) {
       
       # Select CU/UKPDS model
-      if(CU_UKPDS == 'CU') {
+      if(CU_UKPDS == 'JADE') {
             model <- CU_models
       } else if(CU_UKPDS == 'UKPDS'){
             model <- UKPDS_models
@@ -170,12 +191,90 @@ external_validation <- function(outcome, CU_UKPDS, sex) {
       dat <-FUN.deciles_mean_pred(completed)
       library(reshape2)
       dat.melt <- melt(dat, id.vars = 'Deciles', variable.name = 'Group', value.name = 'Risk')
-      
-		p <- plot_cal_bar(dat.melt)
-      
+      p <- plot_cal_bar(dat.melt) + ggtitle(paste(CU_UKPDS, "model"))
       return(list(c_index = c_index, figure = p))
 }
 
+path <- "C:\\Users\\tomli\\Desktop\\Tom folder\\Chao\\Chao - 29Sep2016\\External validation\\CU"
+setwd("E:/vfm")
+#wd_mortality <- "mortality_20170320_5.Rdata" # Mortality data
+wd_mortality <- "mortality_no-insulin_3" # Mortality data
+#wd_mortality <- "mortality_t2_5.Rdata" # Mortality data
+#wd_stroke <- "stroke_20170320_5.Rdata" # Stroke data
+wd_stroke <- "stroke_no-insulin_3" # Stroke data
+#wd_stroke <- "stroke_t2_3.Rdata" # Stroke data
+#wd_chd <- "chd_20170320_5.Rdata" # CHD data
+wd_chd <- "chd_no-insulin_3.Rdata" # CHD data
+#wd_chd <- "chd_t2_5.Rdata" # CHD data
 
+# outcome: 'chd', 'mortality', 'stroke'
+# CU_UKPDS: 'CU', 'UKPDS'
+# sex: 'Female', 'Male', 'All' 
 
+colours <- c('skyblue1','darkblue')
+m_jade <- external_validation(outcome = "mortality", CU_UKPDS = "JADE", sex = "All")
+colours <- c('lightgreen','darkgreen')
+stroke_jade <- external_validation(outcome = "stroke", CU_UKPDS = "JADE", sex = "All")
+stroke_uk <- external_validation(outcome = "stroke", CU_UKPDS = "UKPDS", sex = "All")
+colours <- c('pink','darkred')
+chd_jade <- external_validation(outcome = "chd", CU_UKPDS = "JADE", sex = "All")
+chd_uk <- external_validation(outcome = "chd", CU_UKPDS = "UKPDS", sex = "All")
+
+m_jade$c_index
+stroke_jade$c_index
+stroke_uk$c_index
+chd_uk$c_index
+chd_jade$c_index
+
+# Table of c index
+c_index_all <- expand.grid(outcome = c('mortality','stroke','chd'), 
+                           CU_UKPDS = c('JADE','UKPDS'), 
+                           sex = c('Male','Female','All')) %>%
+  
+  filter(! (outcome == 'mortality' & CU_UKPDS == 'UKPDS')) %>%
+  
+  mutate(c_index = apply(., 1, function(x)
+    external_validation(x[1],x[2],x[3])$c_index))
+
+c_index_all %>%
+  ggplot(aes(x = outcome, y = c_index, fill = CU_UKPDS)) +
+  geom_bar(stat= 'identity', position = 'dodge', width = 0.5) +
+  facet_grid(. ~ sex)
+
+# multiple plot function
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL){
+  library(grid)
+  plots <- c(list(...), plotlist)
+  numPlots = length(plots)
+  if (is.null(layout)) {
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+  if (numPlots==1) {
+    print(plots[[1]])
+    
+  } else {
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    for (i in 1:numPlots) {
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
+
+load("E:/Mortality/all dm egfr sbp lr.Rdata")
+colours <- c('skyblue1','darkblue')
+mortality <- plot_cal_bar(boots_results$calibration) + ggtitle("HKU model")
+load("E:/stroke/all dm creatinine map lr.Rdata")
+colours <- c('lightgreen','darkgreen')
+stroke <- plot_cal_bar(boots_results$calibration) + ggtitle("HKU model")
+load("E:/CHD/all dm creatinine dbp lr.Rdata")
+colours <- c('pink','darkred')
+chd <- plot_cal_bar(boots_results$calibration) + ggtitle("HKU model")
+
+multiplot(mortality, m_jade$figure)
+multiplot(stroke, stroke_jade$figure, stroke_uk$figure)
+multiplot(chd, chd_jade$figure, chd_uk$figure)
 
